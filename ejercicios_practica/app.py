b971096657fa4c13f2e081d51d2cf489e5df77af
Ignacio Serrano <johndoe@example.com>
2022-03-29 17:32:33 -0300

    Practica terminada

--- ejercicios_practica/app.py
+++ ejercicios_practica/app.py
@@ -54,14 +54,20 @@ def personas():
         # Alumno:
         # Implementar la captura de limit y offset de los argumentos
         # de la URL
-        # limit = ...
-        # offset = ....
+        limit_str = str(request.args.get('limit'))
+        offset_str = str(request.args.get('offset'))
 
         # Debe verificar si el limit y offset son válidos cuando
         # no son especificados en la URL
 
         limit = 0
         offset = 0
+
+        if(limit_str is not None) and (limit_str.isdigit()):
+            limit = int(limit_str)
+
+        if(offset_str is not None) and (offset_str.isdigit()):
+            offset = int(offset_str)
 
         result = persona.report(limit=limit, offset=offset)
         return jsonify(result)
@@ -77,13 +83,13 @@ def registro():
             name = ""
             age = 0
             # Alumno:
-            # Obtener del HTTP POST JSON el nombre y los pulsos
-            # name = ...
-            # age = ...
+            # Obtener del HTTP POST JSON el nombre y la edad
+            name = str(request.form.get('name')).lower()
+            age = str(request.form.get('age'))
 
             # Alumno: descomentar la linea persona.insert una vez implementado
             # lo anterior:
-            # persona.insert(name, int(age))
+            persona.insert(name, int(age))
             return Response(status=200)
         except:
             return jsonify({'trace': traceback.format_exc()})
@@ -106,9 +112,9 @@ def comparativa():
 
         # Descomentar luego de haber implementado su función en persona.py:
 
-        # x, y = persona.dashboard()
-        # image_html = utils.graficar(x, y)
-        # return Response(image_html.getvalue(), mimetype='image/png')
+        x, y = persona.dashboard()
+        image_html = utils.graficar(x, y)
+        return Response(image_html.getvalue(), mimetype='image/png')
 
         return "Alumno --> Realice la implementacion"
     except:
@@ -128,4 +134,4 @@ if __name__ == '__main__':
     print('Inove@Server start!')
 
     # Lanzar server
-    app.run(host="127.0.0.1", port=5001)
+    app.run(host="127.0.0.1", port=5000)
